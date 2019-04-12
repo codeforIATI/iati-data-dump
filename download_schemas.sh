7d887ef350e403c28f15762056dbcdb6bc14cccc
Andy Lulham <a.lulham@gmail.com>
2019-04-12 16:15:25 +0100

    Oops – fix path bug with download_schemas

--- download_schemas.sh
+++ download_schemas.sh
@@ -5,6 +5,6 @@ for version in $versions; do
     mkdir -p schemas/$version
     curl -s https://raw.githubusercontent.com/IATI/IATI-Schemas/version-$version/xml.xsd > schemas/$version/xml.xsd
     curl -s https://raw.githubusercontent.com/IATI/IATI-Schemas/version-$version/iati-common.xsd > schemas/$version/iati-common.xsd
-    curl -s https://raw.githubusercontent.com/IATI/IATI-Schemas/version-$version/iati-activities-schema.xsd > ../schemas/$version/iati-activities-schema.xsd
+    curl -s https://raw.githubusercontent.com/IATI/IATI-Schemas/version-$version/iati-activities-schema.xsd > schemas/$version/iati-activities-schema.xsd
     curl -s https://raw.githubusercontent.com/IATI/IATI-Schemas/version-$version/iati-organisations-schema.xsd > schemas/$version/iati-organisations-schema.xsd
 done
