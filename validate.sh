226485cdef296b8ed588c23b2f112cd4f69cdd89
Andy Lulham <a.lulham@gmail.com>
2019-04-12 16:39:48 +0100

    Improve XML error and validation error output

--- validate.sh
+++ validate.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 ## Perform several validation tests on the files in a downloaded data snapshot
 
+IFS=$'\n'
+
 # Check for xml well formedness errors
 rm -f xml-errors
-for f in data/*/*; do
-    echo "Checking XML: $f"
-    # Check file is not empty
-    if [[ -s $f ]]; then
-        xmllint --noout $f 2> /dev/null
-        if [ $? -ne 0 ]; then echo $f >> xml-errors; fi
-    fi
+FILES=urls/*
+for f in $FILES; do
+    for url_line in `cat $f`; do
+        org_name=`basename $f`
+        package_name=`echo $url_line | sed 's/ .*$//'`
+        filename="data/$org_name/$package_name.xml"
+        echo "Checking XML: $package_name"
+        # Check file is not empty
+        if [[ -s $filename ]]; then
+            xmllint --noout $filename 2> /dev/null
+            if [ $? -ne 0 ]; then
+                echo $org_name $url_line >> xml-errors;
+            fi
+        fi
+    done
 done
 # Prevent empty gist that won't be uploaded
 echo "." >> xml-errors
@@ -19,14 +29,21 @@ echo "Downloading Schemas"
 
 # Validate all the files against the relevant schema
 rm -f validation-errors
-for f in data/*/*; do
-    echo "Validating: $f"
-    if [[ -s $f ]]; then
-        topel="`xmllint --xpath "name(/*)" "$f"`"
-        version="`xmllint --xpath "string(/*/@version)" "$f"`"
-        if [ "$version" == "1.01" ] || [ "$version" == "1" ] || [ "$version" == "1.0" ] || [ "$version" == "1.00" ]; then version="1.01"; fi
-        xmllint --noout --schema schemas/$version/$topel-schema.xsd "$f" 2> /dev/null
-        if [ $? -ne 0 ]; then echo $f >> validation-errors; fi
-    fi
+for f in $FILES; do
+    for url_line in `cat $f`; do
+        org_name=`basename $f`
+        package_name=`echo $url_line | sed 's/ .*$//'`
+        filename="data/$org_name/$package_name.xml"
+        echo "Validating: $filename"
+        if [[ -s $filename ]]; then
+            topel="`xmllint --xpath "name(/*)" "$filename"`"
+            version="`xmllint --xpath "string(/*/@version)" "$filename"`"
+            if [ "$version" == "1.01" ] || [ "$version" == "1" ] || [ "$version" == "1.0" ] || [ "$version" == "1.00" ]; then version="1.01"; fi
+            xmllint --noout --schema schemas/$version/$topel-schema.xsd "$filename" 2> /dev/null
+            if [ $? -ne 0 ]; then
+                echo $org_name $url_line >> validation-errors;
+            fi
+        fi
+    done
 done
 echo "." >> validation-errors
