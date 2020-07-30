ce59a460de165f796c25cecc7c438fc087f44f82
Andy Lulham <a.lulham@gmail.com>
2020-07-30 18:26:35 +0100

    Add a --skip-metadata switch for testing

--- grab_urls.py
+++ grab_urls.py
@@ -1,3 +1,4 @@
+import sys
 import json
 from os.path import exists
 from os import makedirs
@@ -18,64 +19,73 @@ def request_with_backoff(*args, attempts=5, backoff=0.5, **kwargs):
     raise Exception(f'Failed after {attempts} attempts. Giving up.')
 
 
-# Loop through each page and save the URL end-points of the data files
-# You may need to set up an empty directory called "urls"
-print('Fetching:')
-api_root = 'https://iatiregistry.org/api/3/action'
-page = 1
-page_size = 1000
+def main(args):
+    skip_metadata = '--skip-metadata' in args
+    if skip_metadata:
+        print('Skipping metadata')
 
-tmpl = 'echo Downloading {package_name} : "{url}" ; ' + \
-       'curl -L -k -f -s ' + \
-       '-H "Accept: application/xhtml+xml, application/xml,*/*;q=0.9" ' + \
-       '--retry 4 --retry-delay 10 -y 30 ' + \
-       '-Y 1000 -A "IATI data dump" ' + \
-       '--create-dirs -o data/{org_name}/{package_name}.xml ' + \
-       '"{url}" 2>&1 >/dev/null ; exitcode=$? ; ' + \
-       'test "$exitcode" != 0 && ' + \
-       'echo $exitcode {org_name} {package_name} {url} > ' + \
-       'logs/{package_name}.log\n'
+    # Loop through each page and save the URL end-points of the data files
+    # You may need to set up an empty directory called "urls"
+    print('Fetching:')
+    api_root = 'https://iatiregistry.org/api/3/action'
+    page = 1
+    page_size = 1000
 
-while True:
-    print(f'Page {page}')
-    start = page_size * (page - 1)
-    result = request_with_backoff(
-        'post',
-        f'{api_root}/package_search',
-        data={'start': start, 'rows': page_size}).json()['result']
-    if result['results'] == []:
-        break
+    tmpl = 'echo Downloading {package_name} : "{url}" ; ' + \
+           'curl -L -k -f -s ' + \
+           '-H "Accept: application/xhtml+xml, application/xml,*/*;q=0.9" ' + \
+           '--retry 4 --retry-delay 10 -y 30 ' + \
+           '-Y 1000 -A "IATI data dump" ' + \
+           '--create-dirs -o data/{org_name}/{package_name}.xml ' + \
+           '"{url}" 2>&1 >/dev/null ; exitcode=$? ; ' + \
+           'test "$exitcode" != 0 && ' + \
+           'echo $exitcode {org_name} {package_name} {url} > ' + \
+           'logs/{package_name}.log\n'
 
-    for package in result['results']:
-        organization = package['organization']
-        if package['resources'] == [] or not organization:
-            continue
-        package_name = package['name']
-        url = package['resources'][0]['url']
-        org_name = organization["name"]
+    while True:
+        print(f'Page {page}')
+        start = page_size * (page - 1)
+        result = request_with_backoff(
+            'post',
+            f'{api_root}/package_search',
+            data={'start': start, 'rows': page_size}).json()['result']
+        if result['results'] == []:
+            break
 
-        metadata_filepath = f'metadata/{org_name}'
-        if not exists(metadata_filepath):
-            makedirs(metadata_filepath)
-            org_metadata = request_with_backoff(
-                'post',
-                f'{api_root}/group_show',
-                data={'id': org_name}).json()['result']
-            org_metadata_file = f'{metadata_filepath}.json'
-            with open(org_metadata_file, 'w') as f:
-                json.dump(org_metadata, f)
-        metadata_file = f'{metadata_filepath}/{package_name}.json'
-        with open(metadata_file, 'w') as f:
-            json.dump(package, f)
+        for package in result['results']:
+            organization = package['organization']
+            if package['resources'] == [] or not organization:
+                continue
+            package_name = package['name']
+            url = package['resources'][0]['url']
+            org_name = organization["name"]
 
-        with open(f'urls/{org_name}', 'a') as f:
-            f.write(f'{package_name} {url}\n')
+            if not skip_metadata:
+                metadata_filepath = f'metadata/{org_name}'
+                if not exists(metadata_filepath):
+                    makedirs(metadata_filepath)
+                    org_metadata = request_with_backoff(
+                        'post',
+                        f'{api_root}/group_show',
+                        data={'id': org_name}).json()['result']
+                    org_metadata_file = f'{metadata_filepath}.json'
+                    with open(org_metadata_file, 'w') as f:
+                        json.dump(org_metadata, f)
+                metadata_file = f'{metadata_filepath}/{package_name}.json'
+                with open(metadata_file, 'w') as f:
+                    json.dump(package, f)
 
-        output = tmpl.format(
-            org_name=org_name,
-            package_name=package_name,
-            url=url,
-        )
-        with open('downloads.curl', 'a') as f:
-            f.write(output)
-    page += 1
+            with open(f'urls/{org_name}', 'a') as f:
+                f.write(f'{package_name} {url}\n')
+
+            output = tmpl.format(
+                org_name=org_name,
+                package_name=package_name,
+                url=url,
+            )
+            with open('downloads.curl', 'a') as f:
+                f.write(output)
+        page += 1
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
