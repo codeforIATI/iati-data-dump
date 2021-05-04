dc6478145e6a1a7b81de529e06ca7377ab883f83
Andy Lulham <a.lulham@gmail.com>
2021-05-04 13:38:54 +0100

    Set a limit on parellel jobs

--- fetch_data.sh
+++ fetch_data.sh
@@ -1,7 +1,7 @@
 if [ "$1" = "debug" ] ; then
     bash downloads.curl
 else
-    cat downloads.curl | sort -R | parallel -j 0
+    cat downloads.curl | sort -R | parallel -j100
 fi
 
 cat logs/* > errors
